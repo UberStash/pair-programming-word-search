926941ea6fcacec14525fa86e1881c369d132575
UberStash <expiredserver@gmail.com>
2020-07-29 22:37:28 +0000

    completed pair programming session

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,49 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const verticalJoinArray = function(letter) {
+  const newArr = [];
 
-module.exports = wordSearch
+  letter.forEach(row => {
+    newArr.push([]);
+  });
+
+  if (letter.length === 1) {
+    return letter[0].map(element => [element]);
+  }
+  for (let row of letter) {
+    row.forEach((element, index) => {
+      newArr[index].push(element);
+    });
+  }
+  console.log(newArr);
+  const newArrJoin = newArr.map(ls => ls.join(''));
+  return newArrJoin;
+};
+
+
+
+const wordSearch = (letters, word) => {
+  const finalVertical = verticalJoinArray(letters);
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  let reverseWord = word.split('').reverse().join('');
+  console.log(reverseWord);
+    
+  let trueFalse = true;
+  for (let l of horizontalJoin) {
+
+    if (l.includes(word) || l.includes(reverseWord)) {
+      return true;
+
+    } else {
+      for (let l of finalVertical) {
+        if (l.includes(word) || l.includes(reverseWord)) {
+          return true;
+        }
+      }
+    }
+    if (!l.includes(word))  {
+      trueFalse = false;
+    }
+  }
+  return trueFalse;
+};
+
+module.exports = wordSearch;
